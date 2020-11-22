2adcaec125bec2580fe66ebcce0328f43ed2f658
Liuweilong1 <1321382166@qq.com>
2020-11-22 15:12:32 +0800

    刘伟龙上传

---
+++ app/src/main/java/com/example/schoolkownclient/Activity/ForgetPwdActivity.java
@@ -0,0 +1,16 @@
+package com.example.schoolkownclient.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.schoolkownclient.R;
+
+public class ForgetPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pwd);
+    }
+}
