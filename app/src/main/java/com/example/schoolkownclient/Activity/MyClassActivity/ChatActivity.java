f3e3c1efcf46fc19c61a90e87e046730d87aa3d6
xuyijiang <1308500752@qq.com>
2020-11-22 17:27:19 +0800

    xyj

---
+++ app/src/main/java/com/example/schoolkownclient/Activity/MyClassActivity/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.schoolkownclient.Activity.MyClassActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.schoolkownclient.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
