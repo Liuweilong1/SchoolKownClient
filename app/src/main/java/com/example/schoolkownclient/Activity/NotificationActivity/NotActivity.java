45edc73430857b856d79ecca3f0e365cb6b4b852
xuyijiang <1308500752@qq.com>
2020-11-23 11:40:47 +0800

    xyj

---
+++ app/src/main/java/com/example/schoolkownclient/Activity/NotificationActivity/NotActivity.java
@@ -0,0 +1,16 @@
+package com.example.schoolkownclient.Activity.NotificationActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.schoolkownclient.R;
+
+public class NotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not);
+    }
+}
