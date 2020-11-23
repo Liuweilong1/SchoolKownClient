8c21d7127d982d16a61ce6527f7537aa5efeb23a
Liuweilong1 <1321382166@qq.com>
2020-11-23 19:09:32 +0800

    刘伟龙上传

--- app/src/main/java/com/example/schoolkownclient/Activity/ParentFragment/CircleFragment.java
+++ app/src/main/java/com/example/schoolkownclient/Activity/ParentFragment/CircleFragment.java
@@ -9,11 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.schoolkownclient.R;
+
 public class CircleFragment extends Fragment {
+    private View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_circle, container, false);
+            //获取控件
+        }
+        return view;
     }
 }
