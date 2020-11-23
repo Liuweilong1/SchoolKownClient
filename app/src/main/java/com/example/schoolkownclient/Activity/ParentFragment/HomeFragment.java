966ea247e855e6998ba6bbe6a414ebf4cfb60266
Liuweilong1 <1321382166@qq.com>
2020-11-23 10:58:59 +0800

    刘伟龙上传

--- app/src/main/java/com/example/schoolkownclient/Activity/ParentFragment/HomeFragment.java
+++ app/src/main/java/com/example/schoolkownclient/Activity/ParentFragment/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,15 +14,31 @@ import androidx.fragment.app.Fragment;
 import com.example.schoolkownclient.R;
 
 public class HomeFragment extends Fragment {
-
+    private SearchView mSearchView;
     private View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (view == null) {
             view = inflater.inflate(R.layout.fragment_home, container, false);
-
+            //获取控件
+            mSearchView = (SearchView) view.findViewById(R.id.searchView);
+            mSearchView.setSubmitButtonEnabled(true);
         }
+        //设置监听事件
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getContext(), query, Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return view;
     }
 
