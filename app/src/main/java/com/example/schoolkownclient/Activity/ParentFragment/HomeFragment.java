75b7cadf5254720a4ef38df4345ed3fe62540b0f
Liuweilong1 <1321382166@qq.com>
2020-11-23 10:05:19 +0800

    刘伟龙上传

--- app/src/main/java/com/example/schoolkownclient/Activity/ParentFragment/HomeFragment.java
+++ app/src/main/java/com/example/schoolkownclient/Activity/ParentFragment/HomeFragment.java
@@ -9,11 +9,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.schoolkownclient.R;
+
 public class HomeFragment extends Fragment {
+
+    private View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        }
+        return view;
     }
+
 }
