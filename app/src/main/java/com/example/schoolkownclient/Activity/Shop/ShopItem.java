7b1a380e36d78aaec02a9d61dbf8a24f6b4d4790
你的名字 <>
2020-11-24 18:22:17 +0800

    李勇见上传

--- app/src/main/java/com/example/schoolkownclient/Activity/Shop/ShopItem.java
+++ app/src/main/java/com/example/schoolkownclient/Activity/Shop/ShopItem.java
@@ -1,25 +1,15 @@
 package com.example.schoolkownclient.Activity.Shop;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import com.example.schoolkownclient.R;
-import com.example.schoolkownclient.bean.JsonDataActivity;
 
-public class ShopItem extends AppCompatActivity implements View.OnClickListener {
+public class ShopItem extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_item);
-        findViewById(R.id.duihuan).setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        startActivity(new Intent(ShopItem.this, JsonDataActivity.class));
-    }
 }
