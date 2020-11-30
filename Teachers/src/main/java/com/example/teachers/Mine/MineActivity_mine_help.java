bfbc939ff41051d5b444ba2405f57a6c08aaee83
123456gaodan <2548055895@qq.com>
2020-11-30 11:56:46 +0800

    高丹上传

---
+++ Teachers/src/main/java/com/example/teachers/Mine/MineActivity_mine_help.java
@@ -0,0 +1,16 @@
+package com.example.teachers.Mine;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.teachers.R;
+
+public class MineActivity_mine_help extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_help);
+    }
+}
