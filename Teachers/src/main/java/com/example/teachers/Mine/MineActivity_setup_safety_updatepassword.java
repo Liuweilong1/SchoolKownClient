a9e8b9e71c79487cc5d57168ad2053960deee37e
123456gaodan <2548055895@qq.com>
2020-12-01 16:15:06 +0800

    高丹上传

--- Teachers/src/main/java/com/example/teachers/Mine/MineActivity_setup_safety_updatepassword.java
+++ Teachers/src/main/java/com/example/teachers/Mine/MineActivity_setup_safety_updatepassword.java
@@ -1,7 +1,12 @@
 package com.example.teachers.Mine;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +17,8 @@ import com.example.teachers.R;
 
 public class MineActivity_setup_safety_updatepassword extends AppCompatActivity {
 
+    private EditText editText;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,6 +32,26 @@ public class MineActivity_setup_safety_updatepassword extends AppCompatActivity 
             public void onClick(View view) {
 
                 finish();
+            }
+        });
+
+        editText =(EditText) findViewById(R.id.ed_password1);
+        checkBox=(CheckBox) findViewById(R.id.cb_password1);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if(isChecked){
+                    //如果选中，显示密码
+                    editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    //否则隐藏密码
+                    editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+                }
+
             }
         });
     }
