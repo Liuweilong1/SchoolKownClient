37a9261cfe1d5e44148bcd582bfc04b37bac49f4
123456gaodan <2548055895@qq.com>
2020-12-01 16:21:44 +0800

    高丹上传

--- Teachers/src/main/java/com/example/teachers/Mine/MineActivity_setup_safety_updatepassword.java
+++ Teachers/src/main/java/com/example/teachers/Mine/MineActivity_setup_safety_updatepassword.java
@@ -17,8 +17,7 @@ import com.example.teachers.R;
 
 public class MineActivity_setup_safety_updatepassword extends AppCompatActivity {
 
-    private EditText editText;
-    private CheckBox checkBox;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,24 +34,6 @@ public class MineActivity_setup_safety_updatepassword extends AppCompatActivity 
             }
         });
 
-        editText =(EditText) findViewById(R.id.ed_password1);
-        checkBox=(CheckBox) findViewById(R.id.cb_password1);
 
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // TODO Auto-generated method stub
-                if(isChecked){
-                    //如果选中，显示密码
-                    editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                }else{
-                    //否则隐藏密码
-                    editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
-
-                }
-
-            }
-        });
     }
 }
