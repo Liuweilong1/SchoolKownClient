851ddc8bade5f354a5fc6d005ea6a12a67b0b693
liyongjian11 <773913064@qq.com>
2020-12-03 08:42:49 +0800

    李勇见上传

--- Parents/src/main/java/com/example/schoolkownclient/Activity/Shop/ShopShop.java
+++ Parents/src/main/java/com/example/schoolkownclient/Activity/Shop/ShopShop.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.schoolkownclient.Entities.Shop;
 import com.example.schoolkownclient.R;
-
-import java.io.Serializable;
 
 public class ShopShop extends AppCompatActivity {
     private ImageView imageView;
@@ -18,6 +18,7 @@ public class ShopShop extends AppCompatActivity {
     private TextView price;
     private TextView scoring;
     private Shop shop;
+    private Button commute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +27,21 @@ public class ShopShop extends AppCompatActivity {
         name = findViewById(R.id.tv_goods);
         price = findViewById(R.id.tv_num);
         scoring = findViewById(R.id.tv_scoring);
-        Intent intent = getIntent();
+        commute = findViewById(R.id.commute);
+        final Intent intent = getIntent();
         shop = (Shop) intent.getSerializableExtra("shop");
         imageView.setImageBitmap(shop.getPicture());
         name.setText(shop.getName());
         price.setText(shop.getPrice());
         scoring.setText(shop.getIntroduction());
+        commute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(ShopShop.this,ConfirmActivity.class);
+                intent1.putExtra("shop",shop);
+                startActivity(intent1);
+            }
+        });
     }
 
 }
